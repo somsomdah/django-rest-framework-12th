c23aec67202b3212a64f37259dc020cdbddc5fc5
somsomdah <somdah98@gmail.com>
2020-12-06 13:20:39 +0900

    Change prod.py

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -5,7 +5,7 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 DEBUG = False  # 꼭 필요합니다.
 
-ALLOWED_HOSTS = secrets['ALLOWED_HOST']
+ALLOWED_HOSTS = secrets['ALLOWED_HOSTS']
 
 DATABASES = {
     'default': secrets['DB_SETTINGS']
